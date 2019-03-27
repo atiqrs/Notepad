350936cc67f187d4eacaca9f76dbc923890a48df
atiqrs <atiqurrahmans12@yahoo.com>
2019-03-27 17:50:13 +0600

    Project is Complete.

--- app/src/main/java/com/example/lab_test2/MainActivity.java
+++ app/src/main/java/com/example/lab_test2/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     static TextView three;
     static TextView four;
     static TextView five;
+    static int num;
     SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +29,39 @@ public class MainActivity extends AppCompatActivity {
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                num = 1;
+                Intent i = new Intent(MainActivity.this,descriptor.class);
+                startActivity(i);
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num = 2;
+                Intent i = new Intent(MainActivity.this,descriptor.class);
+                startActivity(i);
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num = 3;
+                Intent i = new Intent(MainActivity.this,descriptor.class);
+                startActivity(i);
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num = 4;
+                Intent i = new Intent(MainActivity.this,descriptor.class);
+                startActivity(i);
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num = 5;
                 Intent i = new Intent(MainActivity.this,descriptor.class);
                 startActivity(i);
             }
@@ -39,10 +73,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void diplayData() {
         SharedPreferences sp = getSharedPreferences("Note1",MODE_PRIVATE);
-        String show = sp.getString("Title",null);
-        if(show != null){
-            one.setText(show);
+
+        //Keep the values on EditText
+        String a1=sp.getString("T1",null);
+        String b1=sp.getString("D1",null);
+
+        if(a1!=null && b1!=null) {
+            one.setText(sp.getString("T1",null)+"\n\n"+sp.getString("D1",null));
         }
+        else
+            one.setText("");
+        two.setText(sp.getString("T2",null)+"\n\n"+sp.getString("D2",null));
+        three.setText(sp.getString("T3",null)+"\n\n"+sp.getString("D3",null));
+        four.setText(sp.getString("T4",null)+"\n\n"+sp.getString("D4",null));
+
+        String a5=sp.getString("T5",null);
+        String b5=sp.getString("D5",null);
+
+        if(a5!=null && b5!=null) {
+            five.setText(sp.getString("T5",null)+"\n\n"+sp.getString("D5",null));
+        }
+        else
+            one.setText("");
 
     }
 }
--- app/src/main/java/com/example/lab_test2/descriptor.java
+++ app/src/main/java/com/example/lab_test2/descriptor.java
@@ -7,12 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import static com.example.lab_test2.MainActivity.num;
+
 
 public class descriptor extends AppCompatActivity {
 
     final String SHERED_PREF = "Note1";
     EditText title,description;
     Button save;
+    String TitleTag,DescTag;
     SharedPreferences sp;
     SharedPreferences.Editor editor;
     @Override
@@ -27,33 +30,102 @@ public class descriptor extends AppCompatActivity {
         sp = getSharedPreferences(SHERED_PREF, MODE_PRIVATE); //Initialize Shared Preferencces
         editor = sp.edit();
 
+        showData();
+
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 saveData(); //Put the value save and update
                 updateData();
+                MainDataShow();
                 finish();
             }
         });
     }
+
+    private void MainDataShow() {
+
+    }
+
+    private void showData() {
+        if(num == 1){
+            TitleTag = "T1";
+            DescTag = "D1";
+
+            String OneBtnTitle = sp.getString(TitleTag,null);
+            String OneBtnDesc = sp.getString(DescTag,null);
+
+            if (OneBtnTitle != null){
+                title.setText(OneBtnTitle);
+                description.setText(OneBtnDesc);
+            }
+        } else if (num == 2){
+            TitleTag = "T2";
+            DescTag = "D2";
+            String OneBtnTitle = sp.getString(TitleTag,null);
+            String OneBtnDesc = sp.getString(DescTag,null);
+            if (OneBtnTitle != null){
+                title.setText(OneBtnTitle);
+                description.setText(OneBtnDesc);
+            }
+        } else if (num == 3){
+            TitleTag = "T3";
+            DescTag = "D3";
+            String OneBtnTitle = sp.getString(TitleTag,null);
+            String OneBtnDesc = sp.getString(DescTag,null);
+            if (OneBtnTitle != null){
+                title.setText(OneBtnTitle);
+                description.setText(OneBtnDesc);
+            }
+        } else if (num == 4){
+            TitleTag = "T4";
+            DescTag = "D4";
+            String OneBtnTitle = sp.getString(TitleTag,null);
+            String OneBtnDesc = sp.getString(DescTag,null);
+            if (OneBtnTitle != null){
+                title.setText(OneBtnTitle);
+                description.setText(OneBtnDesc);
+            }
+        } else if (num == 5){
+            TitleTag = "T5";
+            DescTag = "D5";
+            String OneBtnTitle = sp.getString(TitleTag,null);
+            String OneBtnDesc = sp.getString(DescTag,null);
+            if (OneBtnTitle != null){
+                title.setText(OneBtnTitle);
+                description.setText(OneBtnDesc);
+            }
+
+        }
+    }
+
     private void saveData() {
 
         String t = title.getText().toString(); //Put the values
         String d = description.getText().toString();
-        editor.putString("Title",t);
-        editor.putString("Desc",d);
+        editor.putString(TitleTag,t);
+        editor.putString(DescTag,d);
 
         editor.apply(); //Apply the change
     } //Save all data on SharedPreferences
 
     private void updateData() {
 
-        String tt = sp.getString("Title",null); //Keep the values on EditText
+        String tt = sp.getString(TitleTag,null); //Keep the values on EditText
         title.setText(tt);
-        String dd = sp.getString("Desc",null);
+        String dd = sp.getString(DescTag,null);
         description.setText(dd);
 
-        MainActivity.one.setText(tt+"\n\n"+dd); //Update Data/values on Luncher Activity
+        if (num == 1)
+            MainActivity.one.setText(tt+"\n\n"+dd); //Update Data/values on Luncher Activity
+        else if (num == 2)
+            MainActivity.two.setText(tt+"\n\n"+dd);
+        else if (num == 3)
+            MainActivity.three.setText(tt+"\n\n"+dd);
+        else if (num == 4)
+            MainActivity.four.setText(tt+"\n\n"+dd);
+        else if (num == 5)
+            MainActivity.five.setText(tt+"\n\n"+dd);
     }
 }
